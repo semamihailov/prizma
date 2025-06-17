a8e474263ef0541b647c6a95fe01207da33f489f
semamihailov <semamihailov5683@gmail.com>
2025-06-17 19:44:16 +0300

    save

--- js/main.js
+++ js/main.js
@@ -1,5 +1,47 @@
 'use strict';
 $(function() {
+    // Исходные стили
+    const closedStyle = {
+        borderRadius: "25px",
+        borderBottomLeftRadius: "25px",
+        borderBottomRightRadius: "25px"
+    };
+    const openedStyle = {
+        borderRadius: "25px 25px 0 0",
+        borderBottomLeftRadius: "0",
+        borderBottomRightRadius: "0"
+    };
 
+    let selectSelected = $(".select-selected");
+    // Инициализация
+    $(".select-selected").css(closedStyle);
 
-})
+    $(".select-selected").click(function(e) {
+        e.stopPropagation();
+        const isOpen = $("#selectOptions").hasClass("show");
+
+        $("#selectOptions").toggleClass("show");
+        $(".custom-arrow").css("transform", isOpen ? "rotate(0)" : "rotate(180deg)");
+        $(".select-selected").css(isOpen ? closedStyle : openedStyle);
+    });
+
+    $(".option").click(function() {
+        const value = $(this).data("value");
+        const text = $(this).text();
+
+        $(".select-placeholder").text(text);
+        $("#genderInput").val(value);
+
+        $("#selectOptions").removeClass("show");
+        $(".custom-arrow").css("transform", "rotate(0)");
+        $(".select-selected").css(closedStyle);
+    });
+
+    $(document).click(function() {
+        if ($("#selectOptions").hasClass("show")) {
+            $("#selectOptions").removeClass("show");
+            $(".custom-arrow").css("transform", "rotate(0)");
+            $(".select-selected").css(closedStyle);
+        }
+    });
+});
